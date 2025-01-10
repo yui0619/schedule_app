4fb302a3df7ece371b8691eab15dfdaf12082a0c
西田祐一郎 <nishidayuichiro@nishidayuuichirounoMacBook-Pro.local>
2025-01-10 22:16:21 +0900

    add/バリデーションの追加

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,13 @@
 class Plan < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :end_date_not_start_date
+  validates :memo, length: { maximum: 500 }
+
+  def end_date_not_start_date
+    errors.add(:end_date, "は開始日より過去の日付を選択できません") unless
+    self.start_date < self.end_date
+  end
 end
+
